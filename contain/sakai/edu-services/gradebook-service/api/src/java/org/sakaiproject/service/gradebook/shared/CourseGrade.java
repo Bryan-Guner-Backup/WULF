6d206faa8faf030a7f4827c6a443f0238abd1814
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-10-29 04:59:21 +0000

    update

---
+++ contain/sakai/edu-services/gradebook-service/api/src/java/org/sakaiproject/service/gradebook/shared/CourseGrade.java
@@ -0,0 +1,50 @@
+package org.sakaiproject.service.gradebook.shared;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+/**
+ * Wrapper for the course grade that contains the the calculated grade (ie 46.67), the mapped grade (ie F) and any entered grade override (ie D-).
+ */
+public class CourseGrade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String enteredGrade;
+	private String calculatedGrade;
+	private String mappedGrade;
+	
+	public CourseGrade() {}
+
+	public String getEnteredGrade() {
+		return enteredGrade;
+	}
+
+	public void setEnteredGrade(String enteredGrade) {
+		this.enteredGrade = enteredGrade;
+	}
+
+	public String getCalculatedGrade() {
+		return calculatedGrade;
+	}
+
+	public void setCalculatedGrade(String calculatedGrade) {
+		this.calculatedGrade = calculatedGrade;
+	}
+
+	public String getMappedGrade() {
+		return mappedGrade;
+	}
+
+	public void setMappedGrade(String mappedGrade) {
+		this.mappedGrade = mappedGrade;
+	}
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+	}
+	
+}

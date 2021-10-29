fabdc8484030313e7f6ac1b846aec36bf7196612
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-10-29 04:52:40 +0000

    update

---
+++ contain/nushell/crates/nu-command/src/commands/dataframe/dummies.rs
@@ -0,0 +1,49 @@
+use crate::prelude::*;
+use nu_engine::WholeStreamCommand;
+use nu_errors::ShellError;
+use nu_protocol::{dataframe::NuDataFrame, Signature};
+
+use super::utils::parse_polars_error;
+
+pub struct DataFrame;
+
+impl WholeStreamCommand for DataFrame {
+    fn name(&self) -> &str {
+        "dataframe to-dummies"
+    }
+
+    fn usage(&self) -> &str {
+        "Creates a new dataframe with dummy variables"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build("dataframe to-dummies")
+    }
+
+    fn run(&self, args: CommandArgs) -> Result<OutputStream, ShellError> {
+        command(args)
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![Example {
+            description: "Create new dataframe with dummy variables",
+            example: "[[a b]; [1 2] [3 4]] | dataframe to-df | dataframe to-dummies",
+            result: None,
+        }]
+    }
+}
+
+fn command(mut args: CommandArgs) -> Result<OutputStream, ShellError> {
+    let tag = args.call_info.name_tag.clone();
+
+    let df = NuDataFrame::try_from_stream(&mut args.input, &tag.span)?;
+    let res = df.as_ref().to_dummies().map_err(|e| {
+        parse_polars_error(
+            &e,
+            &tag.span,
+            Some("The only allowed column types for dummies are String or Int"),
+        )
+    })?;
+
+    Ok(OutputStream::one(NuDataFrame::dataframe_to_value(res, tag)))
+}

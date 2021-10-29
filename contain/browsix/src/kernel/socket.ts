f367fab0eaa5ec04d6adacef8d8eaa6ff2fd4d88
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-10-29 04:42:19 +0000

    update

---
+++ contain/browsix/src/kernel/socket.ts
@@ -0,0 +1,156 @@
+// Copyright 2016 The Browsix Authors. All rights reserved.
+// Use of this source code is governed by the ISC
+// license that can be found in the LICENSE file.
+
+/// <reference path="../../typings/node/node.d.ts" />
+
+'use strict';
+
+import { EINVAL } from './constants';
+import { ConnectCallback, SyscallContext, IFile, ITask } from './types';
+import { Pipe } from './pipe';
+
+export interface AcceptCallback {
+	(err: any, s?: SocketFile, remoteAddr?: string, remotePort?: number): void;
+}
+
+export function isSocket(f: IFile): f is SocketFile {
+	return f instanceof SocketFile;
+}
+
+export interface Incoming {
+	s: SocketFile;
+	addr: string;
+	port: number;
+	cb: ConnectCallback;
+}
+
+export class SocketFile implements IFile {
+	task:          ITask;
+	isListening:   boolean    = false;
+	parent:        SocketFile = undefined;
+	refCount:      number     = 1;
+
+	port:          number;
+	addr:          string;
+
+	outgoing:      Pipe = undefined;
+	incoming:      Pipe = undefined;
+
+	incomingQueue: Incoming[] = [];
+	acceptQueue:   AcceptCallback[] = [];
+
+	constructor(task: ITask) {
+		this.task = task;
+	}
+
+	stat(cb: (err: any, stats: any) => void): void {
+		throw new Error('TODO: SocketFile.stat not implemented');
+	}
+
+	readdir(cb: (err: any, files: string[]) => void): void {
+		setTimeout(cb, 0, 'cant readdir on normal file');
+	}
+
+	listen(cb: (err: any) => void): void {
+		this.isListening = true;
+		cb(undefined);
+	}
+
+	accept(cb: AcceptCallback): void {
+		if (!this.incomingQueue.length) {
+			this.acceptQueue.push(cb);
+			return;
+		}
+
+		let queued = this.incomingQueue.shift();
+
+		let remote = queued.s;
+		let local = new SocketFile(this.task);
+
+		let outgoing = new Pipe();
+		let incoming = new Pipe();
+
+		local.outgoing = outgoing;
+		remote.incoming = outgoing;
+
+		local.incoming = incoming;
+		remote.outgoing = incoming;
+
+		cb(null, local, queued.addr, queued.port);
+		queued.cb(null);
+	}
+
+	doAccept(remote: SocketFile, remoteAddr: string, remotePort: number, cb: ConnectCallback): void {
+		if (!this.acceptQueue.length) {
+			this.incomingQueue.push({
+				s: remote,
+				addr: remoteAddr,
+				port: remotePort,
+				cb: cb,
+			});
+			return;
+		}
+
+		let acceptCB = this.acceptQueue.shift();
+
+		let local = new SocketFile(this.task);
+
+		let outgoing = new Pipe();
+		let incoming = new Pipe();
+
+		local.outgoing = outgoing;
+		remote.incoming = outgoing;
+
+		local.incoming = incoming;
+		remote.outgoing = incoming;
+
+		acceptCB(null, local, remoteAddr, remotePort);
+		cb(null);
+	}
+
+	connect(addr: string, port: number, cb: ConnectCallback): void {
+		this.task.kernel.connect(this, addr, port, cb);
+	}
+
+	write(buf: string|Buffer, cb: (err: any, len?: number) => void): void {
+		if (typeof buf === 'string')
+			this.outgoing.write(buf);
+		else
+			this.outgoing.writeBuffer((<Buffer>buf));
+		cb = arguments[arguments.length-1];
+		cb(undefined, buf.length);
+	}
+
+	read(buf: Buffer, pos: number, len: number, off: number, cb: (err: any, len?: number) => void): void {
+		this.incoming.read(buf, pos, len, off, cb);
+	}
+
+	readSync(): Buffer {
+		return this.incoming.readSync();
+	}
+
+	llseek(offhi: number, offlo: number, whence: number, cb: (err: number, off: number) => void): void {
+		cb(-EINVAL, undefined);
+	}
+
+	ref(): void {
+		this.refCount++;
+		if (this.outgoing)
+			this.outgoing.ref();
+		if (this.incoming)
+			this.incoming.ref();
+	}
+
+	unref(): void {
+		if (this.outgoing)
+			this.outgoing.unref();
+		if (this.incoming)
+			this.incoming.unref();
+		this.refCount--;
+		if (!this.refCount) {
+			if (this.port && this.addr)
+				this.task.kernel.unbind(this, this.addr, this.port);
+		}
+	}
+}
---
+++ contain/browsix/src/kernel/types.ts
@@ -0,0 +1,121 @@
+// Copyright 2016 UMass Amherst. All rights reserved.
+// Use of this source code is governed by the ISC
+// license that can be found in the LICENSE file.
+
+/// <reference path="../../typings/node/node.d.ts" />
+
+'use strict';
+
+export interface ExitCallback {
+	(pid: number, code: number): void;
+}
+
+export interface OutputCallback {
+	// TODO: change from string to Buffer
+	(pid: number, output: string): void;
+}
+
+export interface SyscallResult {
+	id: number;
+	name: string;
+	args: any[];
+}
+
+export interface ConnectCallback {
+	(err: any): void;
+}
+
+export interface IKernel {
+	fs: any; // FIXME
+
+	nCPUs: number;
+	debug: boolean;
+
+	system(cmd: string, onExit: ExitCallback, onStdout: OutputCallback, onStderr: OutputCallback): void;
+	exit(task: ITask, code: number): void;
+	wait(pid: number): void;
+	doSyscall(syscall: Syscall): void;
+	connect(s: IFile, addr: string, port: number, cb: ConnectCallback): void;
+	unbind(s: IFile, addr: string, port: number): any;
+
+	once(event: string, cb: Function): any;
+}
+
+export interface Environment {
+	[name: string]: string;
+}
+
+export interface IFile {
+
+	write(buf: string|Buffer, cb: (err: any, len?: number) => void): void;
+	read(buf: Buffer, pos: number, len: number, off: number, cb: (err: any, len?: number) => void): void;
+	stat(cb: (err: any, stats: any) => void): void;
+	llseek(offhi: number, offlo: number, whence: number, cb: (err: number, off: number) => void): void;
+	readdir(cb: (err: any, files: string[]) => void): void;
+
+	ref(): void;
+	unref(): void;
+}
+
+export interface ITask {
+	kernel: IKernel;
+	parent: ITask;
+	worker: Worker;
+
+	pid: number;
+	files: {[n: number]: any; };  // TODO: should be IFile
+
+	exitCode: number;
+
+	exePath: string;
+	args: string[];
+	env: Environment;
+	cwd: string;
+	priority: number;
+
+	exec(filename: string, args: string[], env: Environment, cb: (err: any, pid: number) => void): void;
+	allocFD(): number;
+	addFile(f: IFile): number;
+	schedule(msg: SyscallResult): void;
+	setPriority(prio: number): number;
+	wait4(ctx: SyscallContext, pid: number, options: number): void;
+	chdir(path: string, cb: Function): void;
+}
+
+export class SyscallContext {
+	constructor(
+		public task: ITask,
+		public id:   number) {}
+
+	complete(...args: any[]): void {
+		this.task.schedule({
+			id: this.id,
+			name: undefined,
+			args: args,
+		});
+	}
+}
+
+export class Syscall {
+	constructor(
+		public ctx:  SyscallContext,
+		public name: string,
+		public args: any[]) {}
+
+	private static requiredOnData: string[] = ['id', 'name', 'args'];
+
+	static From(task: ITask, ev: MessageEvent): Syscall {
+		if (!ev.data)
+			return;
+		for (let i = 0; i < Syscall.requiredOnData.length; i++) {
+			if (!ev.data.hasOwnProperty(Syscall.requiredOnData[i]))
+				return;
+		}
+		let ctx = new SyscallContext(task, ev.data.id);
+		return new Syscall(ctx, ev.data.name, ev.data.args);
+	}
+
+	callArgs(): any[] {
+		return [this.ctx].concat(this.args);
+	}
+}

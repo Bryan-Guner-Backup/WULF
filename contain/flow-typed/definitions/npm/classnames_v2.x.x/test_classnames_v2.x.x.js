18f5925a677fce76e41153fcb08f30ee75e799df
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-10-29 04:45:49 +0000

    update

---
+++ contain/flow-typed/definitions/npm/classnames_v2.x.x/test_classnames_v2.x.x.js
@@ -0,0 +1,18 @@
+// @flow
+
+import classnames from "classnames";
+
+classnames();
+classnames('a');
+classnames('a', 'b');
+classnames({a: true});
+classnames({a: true}, {b: true});
+classnames('a', {b: true});
+classnames({a: true}, 'b');
+classnames({a: null, b: undefined});
+classnames(undefined);
+classnames(null);
+classnames('a', false);
+
+// $ExpectError
+classnames(42);

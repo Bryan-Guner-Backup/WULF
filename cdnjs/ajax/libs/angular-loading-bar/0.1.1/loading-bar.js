fc61835981d81a3b65e5fe8487e943670964e3af
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-10-29 07:16:29 +0000

    -update

--- cdnjs/ajax/libs/angular-loading-bar/0.1.1/loading-bar.js
+++ cdnjs/ajax/libs/angular-loading-bar/0.1.1/loading-bar.js
@@ -1,4 +1,4 @@
-/*! 
+/*!
  * angular-loading-bar v0.1.1
  * https://chieffancypants.github.io/angular-loading-bar
  * Copyright (c) 2013 Wes Cruver
@@ -14,260 +14,271 @@
  * License: MIT
  */
 
+(function () {
+  "use strict";
 
-(function() {
-'use strict';
-
-angular.module('angular-loading-bar', ['chieffancypants.loadingBar']);
-
-/**
- * loadingBarInterceptor service
- *
- * Registers itself as an Angular interceptor and listens for XHR requests.
- */
-angular.module('chieffancypants.loadingBar', [])
-  .config(['$httpProvider', function ($httpProvider) {
-
-    var interceptor = ['$q', '$cacheFactory', 'cfpLoadingBar', function ($q, $cacheFactory, cfpLoadingBar) {
-
-      /**
-       * The total number of requests made
-       */
-      var reqsTotal = 0;
-
-      /**
-       * The number of requests completed (either successfully or not)
-       */
-      var reqsCompleted = 0;
-
-
-      /**
-       * calls cfpLoadingBar.complete() which removes the
-       * loading bar from the DOM.
-       */
-      function setComplete() {
-        cfpLoadingBar.complete();
-        reqsCompleted = 0;
-        reqsTotal = 0;
-      }
-
-      /**
-       * Determine if the response has already been cached
-       * @param  {Object}  config the config option from the request
-       * @return {Boolean} retrns true if cached, otherwise false
-       */
-      function isCached(config) {
-        var cache;
-        var defaults = $httpProvider.defaults;
-
-        if (config.method !== 'GET' || config.cache === false) {
-          config.cached = false;
-          return false;
-        }
-
-        if (config.cache === true && defaults.cache === undefined) {
-          cache = $cacheFactory.get('$http');
-        } else if (defaults.cache !== undefined) {
-          cache = defaults.cache;
-        } else {
-          cache = config.cache;
-        }
-
-        var cached = cache !== undefined ?
-          cache.get(config.url) !== undefined : false;
-
-        if (config.cached !== undefined && cached !== config.cached) {
-          return config.cached;
-        }
-        config.cached = cached;
-        return cached;
-      }
-
-      return {
-        'request': function(config) {
-          // Check to make sure this request hasn't already been cached and that
-          // the requester didn't explicitly ask us to ignore this request:
-          if (!config.ignoreLoadingBar && !isCached(config)) {
-            if (reqsTotal === 0) {
-              cfpLoadingBar.start();
-            }
-            reqsTotal++;
-          }
-          return config;
-        },
-
-        'response': function(response) {
-          if (!isCached(response.config)) {
-            reqsCompleted++;
-            if (reqsCompleted >= reqsTotal) {
-              setComplete();
-            } else {
-              cfpLoadingBar.set(reqsCompleted / reqsTotal);
-            }
-          }
-          return response;
-        },
-
-        'responseError': function(rejection) {
-          if (!isCached(rejection.config)) {
-            reqsCompleted++;
-            if (reqsCompleted >= reqsTotal) {
-              setComplete();
-            } else {
-              cfpLoadingBar.set(reqsCompleted / reqsTotal);
-            }
-          }
-          return $q.reject(rejection);
-        }
-      };
-    }];
-
-    $httpProvider.interceptors.push(interceptor);
-  }])
-
+  angular.module("angular-loading-bar", ["chieffancypants.loadingBar"]);
 
   /**
-   * Loading Bar
+   * loadingBarInterceptor service
    *
-   * This service handles adding and removing the actual element in the DOM.
-   * Generally, best practices for DOM manipulation is to take place in a
-   * directive, but because the element itself is injected in the DOM only upon
-   * XHR requests, and it's likely needed on every view, the best option is to
-   * use a service.
+   * Registers itself as an Angular interceptor and listens for XHR requests.
    */
-  .provider('cfpLoadingBar', function() {
+  angular
+    .module("chieffancypants.loadingBar", [])
+    .config([
+      "$httpProvider",
+      function ($httpProvider) {
+        var interceptor = [
+          "$q",
+          "$cacheFactory",
+          "cfpLoadingBar",
+          function ($q, $cacheFactory, cfpLoadingBar) {
+            /**
+             * The total number of requests made
+             */
+            var reqsTotal = 0;
 
-    this.includeSpinner = true;
-    this.includeBar = true;
-    this.parentSelector = 'body';
+            /**
+             * The number of requests completed (either successfully or not)
+             */
+            var reqsCompleted = 0;
 
-    this.$get = ['$document', '$timeout', '$animate', '$rootScope', function ($document, $timeout, $animate, $rootScope) {
+            /**
+             * calls cfpLoadingBar.complete() which removes the
+             * loading bar from the DOM.
+             */
+            function setComplete() {
+              cfpLoadingBar.complete();
+              reqsCompleted = 0;
+              reqsTotal = 0;
+            }
 
-      var $parentSelector = this.parentSelector,
-        $parent = $document.find($parentSelector),
-        loadingBarContainer = angular.element('<div id="loading-bar"><div class="bar"><div class="peg"></div></div></div>'),
-        loadingBar = loadingBarContainer.find('div').eq(0),
-        spinner = angular.element('<div id="loading-bar-spinner"><div class="spinner-icon"></div></div>');
+            /**
+             * Determine if the response has already been cached
+             * @param  {Object}  config the config option from the request
+             * @return {Boolean} retrns true if cached, otherwise false
+             */
+            function isCached(config) {
+              var cache;
+              var defaults = $httpProvider.defaults;
 
-      var incTimeout,
-        completeTimeout,
-        started = false,
-        status = 0;
+              if (config.method !== "GET" || config.cache === false) {
+                config.cached = false;
+                return false;
+              }
 
-      var includeSpinner = this.includeSpinner;
-      var includeBar = this.includeBar;
+              if (config.cache === true && defaults.cache === undefined) {
+                cache = $cacheFactory.get("$http");
+              } else if (defaults.cache !== undefined) {
+                cache = defaults.cache;
+              } else {
+                cache = config.cache;
+              }
 
-      /**
-       * Inserts the loading bar element into the dom, and sets it to 2%
-       */
-      function _start() {
-        $timeout.cancel(completeTimeout);
+              var cached =
+                cache !== undefined
+                  ? cache.get(config.url) !== undefined
+                  : false;
 
-        // do not continually broadcast the started event:
-        if (started) {
-          return;
-        }
+              if (config.cached !== undefined && cached !== config.cached) {
+                return config.cached;
+              }
+              config.cached = cached;
+              return cached;
+            }
 
-        $rootScope.$broadcast('cfpLoadingBar:started');
-        started = true;
+            return {
+              request: function (config) {
+                // Check to make sure this request hasn't already been cached and that
+                // the requester didn't explicitly ask us to ignore this request:
+                if (!config.ignoreLoadingBar && !isCached(config)) {
+                  if (reqsTotal === 0) {
+                    cfpLoadingBar.start();
+                  }
+                  reqsTotal++;
+                }
+                return config;
+              },
 
-        if (includeBar) {
-          $animate.enter(loadingBarContainer, $parent);
-        }
+              response: function (response) {
+                if (!isCached(response.config)) {
+                  reqsCompleted++;
+                  if (reqsCompleted >= reqsTotal) {
+                    setComplete();
+                  } else {
+                    cfpLoadingBar.set(reqsCompleted / reqsTotal);
+                  }
+                }
+                return response;
+              },
 
-        if (includeSpinner) {
-          $animate.enter(spinner, $parent);
-        }
-        _set(0.02);
-      }
+              responseError: function (rejection) {
+                if (!isCached(rejection.config)) {
+                  reqsCompleted++;
+                  if (reqsCompleted >= reqsTotal) {
+                    setComplete();
+                  } else {
+                    cfpLoadingBar.set(reqsCompleted / reqsTotal);
+                  }
+                }
+                return $q.reject(rejection);
+              },
+            };
+          },
+        ];
 
-      /**
-       * Set the loading bar's width to a certain percent.
-       *
-       * @param n any value between 0 and 1
-       */
-      function _set(n) {
-        if (!started) {
-          return;
-        }
-        var pct = (n * 100) + '%';
-        loadingBar.css('width', pct);
-        status = n;
+        $httpProvider.interceptors.push(interceptor);
+      },
+    ])
 
-        // increment loadingbar to give the illusion that there is always
-        // progress but make sure to cancel the previous timeouts so we don't
-        // have multiple incs running at the same time.
-        $timeout.cancel(incTimeout);
-        incTimeout = $timeout(function() {
-          _inc();
-        }, 250);
-      }
+    /**
+     * Loading Bar
+     *
+     * This service handles adding and removing the actual element in the DOM.
+     * Generally, best practices for DOM manipulation is to take place in a
+     * directive, but because the element itself is injected in the DOM only upon
+     * XHR requests, and it's likely needed on every view, the best option is to
+     * use a service.
+     */
+    .provider("cfpLoadingBar", function () {
+      this.includeSpinner = true;
+      this.includeBar = true;
+      this.parentSelector = "body";
 
-      /**
-       * Increments the loading bar by a random amount
-       * but slows down as it progresses
-       */
-      function _inc() {
-        if (_status() >= 1) {
-          return;
-        }
+      this.$get = [
+        "$document",
+        "$timeout",
+        "$animate",
+        "$rootScope",
+        function ($document, $timeout, $animate, $rootScope) {
+          var $parentSelector = this.parentSelector,
+            $parent = $document.find($parentSelector),
+            loadingBarContainer = angular.element(
+              '<div id="loading-bar"><div class="bar"><div class="peg"></div></div></div>'
+            ),
+            loadingBar = loadingBarContainer.find("div").eq(0),
+            spinner = angular.element(
+              '<div id="loading-bar-spinner"><div class="spinner-icon"></div></div>'
+            );
 
-        var rnd = 0;
-
-        // TODO: do this mathmatically instead of through conditions
-
-        var stat = _status();
-        if (stat >= 0 && stat < 0.25) {
-          // Start out between 3 - 6% increments
-          rnd = (Math.random() * (5 - 3 + 1) + 3) / 100;
-        } else if (stat >= 0.25 && stat < 0.65) {
-          // increment between 0 - 3%
-          rnd = (Math.random() * 3) / 100;
-        } else if (stat >= 0.65 && stat < 0.9) {
-          // increment between 0 - 2%
-          rnd = (Math.random() * 2) / 100;
-        } else if (stat >= 0.9 && stat < 0.99) {
-          // finally, increment it .5 %
-          rnd = 0.005;
-        } else {
-          // after 99%, don't increment:
-          rnd = 0;
-        }
-
-        var pct = _status() + rnd;
-        _set(pct);
-      }
-
-      function _status() {
-        return status;
-      }
-
-      function _complete() {
-        $rootScope.$broadcast('cfpLoadingBar:completed');
-        _set(1);
-
-        // Attempt to aggregate any start/complete calls within 500ms:
-        completeTimeout = $timeout(function() {
-          $animate.leave(loadingBarContainer, function() {
+          var incTimeout,
+            completeTimeout,
+            started = false,
             status = 0;
-            started = false;
-          });
-          $animate.leave(spinner);
-        }, 500);
-      }
 
-      return {
-        start          : _start,
-        set            : _set,
-        status         : _status,
-        inc            : _inc,
-        complete       : _complete,
-        includeSpinner : this.includeSpinner,
-        parentSelector : this.parentSelector
-      };
+          var includeSpinner = this.includeSpinner;
+          var includeBar = this.includeBar;
 
+          /**
+           * Inserts the loading bar element into the dom, and sets it to 2%
+           */
+          function _start() {
+            $timeout.cancel(completeTimeout);
 
+            // do not continually broadcast the started event:
+            if (started) {
+              return;
+            }
 
-    }];     //
-  });       // wtf javascript. srsly
-})();       //
+            $rootScope.$broadcast("cfpLoadingBar:started");
+            started = true;
+
+            if (includeBar) {
+              $animate.enter(loadingBarContainer, $parent);
+            }
+
+            if (includeSpinner) {
+              $animate.enter(spinner, $parent);
+            }
+            _set(0.02);
+          }
+
+          /**
+           * Set the loading bar's width to a certain percent.
+           *
+           * @param n any value between 0 and 1
+           */
+          function _set(n) {
+            if (!started) {
+              return;
+            }
+            var pct = n * 100 + "%";
+            loadingBar.css("width", pct);
+            status = n;
+
+            // increment loadingbar to give the illusion that there is always
+            // progress but make sure to cancel the previous timeouts so we don't
+            // have multiple incs running at the same time.
+            $timeout.cancel(incTimeout);
+            incTimeout = $timeout(function () {
+              _inc();
+            }, 250);
+          }
+
+          /**
+           * Increments the loading bar by a random amount
+           * but slows down as it progresses
+           */
+          function _inc() {
+            if (_status() >= 1) {
+              return;
+            }
+
+            var rnd = 0;
+
+            // TODO: do this mathmatically instead of through conditions
+
+            var stat = _status();
+            if (stat >= 0 && stat < 0.25) {
+              // Start out between 3 - 6% increments
+              rnd = (Math.random() * (5 - 3 + 1) + 3) / 100;
+            } else if (stat >= 0.25 && stat < 0.65) {
+              // increment between 0 - 3%
+              rnd = (Math.random() * 3) / 100;
+            } else if (stat >= 0.65 && stat < 0.9) {
+              // increment between 0 - 2%
+              rnd = (Math.random() * 2) / 100;
+            } else if (stat >= 0.9 && stat < 0.99) {
+              // finally, increment it .5 %
+              rnd = 0.005;
+            } else {
+              // after 99%, don't increment:
+              rnd = 0;
+            }
+
+            var pct = _status() + rnd;
+            _set(pct);
+          }
+
+          function _status() {
+            return status;
+          }
+
+          function _complete() {
+            $rootScope.$broadcast("cfpLoadingBar:completed");
+            _set(1);
+
+            // Attempt to aggregate any start/complete calls within 500ms:
+            completeTimeout = $timeout(function () {
+              $animate.leave(loadingBarContainer, function () {
+                status = 0;
+                started = false;
+              });
+              $animate.leave(spinner);
+            }, 500);
+          }
+
+          return {
+            start: _start,
+            set: _set,
+            status: _status,
+            inc: _inc,
+            complete: _complete,
+            includeSpinner: this.includeSpinner,
+            parentSelector: this.parentSelector,
+          };
+        },
+      ]; //
+    }); // wtf javascript. srsly
+})(); //

d89a5754dee4512990b7c8429bc335e57c12a5d2
Bryan-Guner-Backup <bgoonz4212@gmail.com>
2021-10-29 04:28:31 +0000

    update

---
+++ contain/brave-browser/lib/test.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+const config = require('../lib/config')
+const util = require('../lib/util')
+
+const test = (suite, buildConfig = config.defaultBuildConfig, options) => {
+  config.buildConfig = buildConfig
+  config.update(options)
+
+  const braveArgs = [
+    '--enable-logging',
+    '--v=' + options.v,
+  ]
+
+  if (options.filter) {
+    braveArgs.push('--gtest_filter=' + options.filter)
+  }
+
+  // Build the tests
+  util.run('ninja', ['-C', config.outputDir, suite], config.defaultOptions)
+
+  let testBinary;
+  if (process.platform === 'win32') {
+    testBinary = `${suite}.exe`
+  } else {
+    testBinary = suite
+  }
+
+  // Run the tests
+  util.run(path.join(config.outputDir, testBinary), braveArgs, config.defaultOptions)
+}
+
+module.exports = test
